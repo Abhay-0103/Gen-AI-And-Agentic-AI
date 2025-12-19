109648f448f86223430b6b85959fd3a3bbf1cf42
Abhay-0103 <ab0321054@gmail.com>
2025-12-19 21:29:22 +0530

    Immutable

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,10 @@
+# Immutable Data Types
+
+sugar_amout = 2
+print(f"Initial sugar: {sugar_amout}")
+
+sugar_amout = 12
+print(f"Second sugar: {sugar_amout}")
+
+print(f"ID of 2: {id(2)}")
+print(f"ID of 12: {id(12)}")
