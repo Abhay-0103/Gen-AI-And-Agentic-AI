1f7198b649f3b7946e1a8444f87c86b52fc6ed28
Abhay-0103 <ab0321054@gmail.com>
2025-12-19 21:29:33 +0530

    Mutable

---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,11 @@
+# Mutable Data Types 
+
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+
+spice_mix.add("Ginger")
+spice_mix.add("cardamom")
+
+print(f"Initial spice mix id: {spice_mix}")
+print(f"After spice mix id: {id(spice_mix)}")
