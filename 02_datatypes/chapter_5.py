15d0b5d75c3bdcdffe9a325b55f6745bc17453bc
Abhay-0103 <ab0321054@gmail.com>
2025-12-19 22:17:00 +0530

    Add chapter 5: Introduction to real numbers with temperature examples

---
+++ 02_datatypes/chapter_5.py
@@ -0,0 +1,16 @@
+import sys
+from fractions import Fraction
+from decimal import Decimal
+
+# Real Numbers
+
+ideal_temp = 95.5
+current_temp = 95.49
+
+print(f"Ideal Temperature: {ideal_temp}")
+
+print(f"Current Temperature: {current_temp}")
+
+print(f"Difference temp {ideal_temp - current_temp}")
+
+print(sys.float_info)
