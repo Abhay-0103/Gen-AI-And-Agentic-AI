f283df48957bcc1e65613ec036500bf4a07dc513
Abhay-0103 <ab0321054@gmail.com>
2025-12-19 21:50:08 +0530

    Add chapter 3: Arithmetic operations and large integers examples

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,48 @@
+# Interger
+
+
+# Arithmetic Operations
+
+black_tea_grams = 14
+gineger_grams = 3
+
+total_grams = black_tea_grams + gineger_grams
+print(f"Total grams of tea: {total_grams}")
+
+# Subtraction
+
+remaing_tea = black_tea_grams - gineger_grams
+print(f"Remaing tea grams: {remaing_tea}")
+
+# Multiplication
+
+milk_litres = 7
+servings = 4
+milk_per_serving = milk_litres / servings
+print(f"Milk per serving: {milk_per_serving} litres")
+
+# Floor Division, Modulus and Exponentiation
+
+total_tea_bags = 7
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"Bags per pot: {bags_per_pot}")
+
+# Modulus
+
+total_cadamom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cadamom_pods % pods_per_cup
+print(f"Leftover pods: {leftover_pods}")
+
+
+# Exponentiation
+
+base_flavor_stranght = 2
+scale_factorre = 3
+powerfull_flavor = base_flavor_stranght ** scale_factorre
+print(f"Powerfull flavor strenght: {powerfull_flavor}")
+
+# Large Integers
+total_tea_leaves_harvested = 1_000_000_000
+print(f"Total tea leaves harvested: {total_tea_leaves_harvested}")
