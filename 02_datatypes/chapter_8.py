c8c0c9cbe6ee929c6aef72b6a890c32d9067e389
Abhay-0103 <ab0321054@gmail.com>
2025-12-21 21:08:54 +0530

    Add examples for operator overloading, array operations, and string conversion in chapter 8

--- 02_datatypes/chapter_8.py
+++ 02_datatypes/chapter_8.py
@@ -33,3 +33,19 @@ print(f"Sorted Chai Ingredients are : {chai_ingredients}")
 sugar_level = [1,2,3,4,5]
 print(f"Maximum sugar level is {max(sugar_level)}")
 print(f"Minimum sugar level is {min(sugar_level)}")
+
+# Operator Overloading
+base_liquids = ["water", "milk"]
+extra_flavors = ["ginger"]
+
+full_liquids_mix = base_liquids + extra_flavors
+print(f"Full liquids mix: {full_liquids_mix}")
+
+# Array in Operations Overloading
+strong_brew = ["black tea", "water"] * 3
+print(f"Strong brew mix: {strong_brew}")
+
+# String Covtering into list
+raw_spice_data = bytearray(b"Cinnamon")
+raw_spice_data = raw_spice_data.replace(b"Cinna", b"Card")
+print(f"Raw spice data as list: {raw_spice_data}")
