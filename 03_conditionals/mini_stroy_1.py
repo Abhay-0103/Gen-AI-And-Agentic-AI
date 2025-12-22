2146c3573f8503139cbb685ffd82a6a576e1a7a9
Abhay-0103 <ab0321054@gmail.com>
2025-12-22 19:54:52 +0530

    Implement kettle boiling check with conditional statements

---
+++ 03_conditionals/mini_stroy_1.py
@@ -0,0 +1,8 @@
+# Conditional statement to check if the kettle is boiled
+
+kettle_boiled = True
+
+if kettle_boiled:
+    print("kettle Done! Time to make Chai")
+else:
+    print("kettle not done yet, please wait")
